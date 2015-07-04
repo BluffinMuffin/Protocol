32d04757636730498dcf53fc42f54ff9a4e421d1
Eric Masse <ericmas001@hotmail.com>
2015-07-04 14:59:28 -0400

    Adding some event handling for easier implementation

---
+++ C#/BluffinMuffin.Protocol.DataTypes/EventHandling/SeatEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluffinMuffin.Protocol.DataTypes.EventHandling
+{
+    public class SeatEventArgs : EventArgs
+    {
+        private readonly SeatInfo m_Seat;
+        public SeatInfo Seat { get { return m_Seat; } }
+
+        public SeatEventArgs(SeatInfo s)
+        {
+            m_Seat = s;
+        }
+    }
+}
