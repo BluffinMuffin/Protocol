2cb872f8fadc4a05ffd83cc4b9471fbf4e7eaa90
Éric Massé <Ericmas001@gmail.com>
2015-09-06 15:26:06 -0400

    Making command_decoder working again

--- Python/bluffinmuffin/protocol/command_decoder.py
+++ Python/bluffinmuffin/protocol/command_decoder.py
@@ -13,15 +13,16 @@ class CommandDecoder:
 
     @classmethod
     def decode(cls, obj):
+        main_prefix = 'bluffinmuffin.protocol'
         prefixes = [
             '',
-            'lobby',
-            'lobby.quick_mode',
-            'lobby.registered_mode',
-            'game'
+            '.lobby',
+            '.lobby.quick_mode',
+            '.lobby.registered_mode',
+            '.game'
         ]
         for p in prefixes:
-            res = cls._decode_class(obj, obj['CommandName'], '{0}.{1}'.format(p, re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', obj['CommandName'])).lower()))
+            res = cls._decode_class(obj, obj['CommandName'], '{0}{1}.{2}'.format(main_prefix, p, re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', obj['CommandName'])).lower()))
             if res is not None:
                 return res
         return None
