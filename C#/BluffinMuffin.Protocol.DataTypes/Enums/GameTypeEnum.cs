deab30550d7d2855109faf4d142098894fb2fc82
Éric Massé <Ericmas001@gmail.com>
2015-09-07 13:06:46 -0400

    Adding GameTypeEnums

--- C#/BluffinMuffin.Protocol.DataTypes/Enums/GameTypeEnum.cs
+++ C#/BluffinMuffin.Protocol.DataTypes/Enums/GameTypeEnum.cs
@@ -6,8 +6,19 @@
     public enum GameTypeEnum
     {
         /// <summary>
-        /// Holdem
+        /// Games in which players are dealt a complete hand, hidden, and then improve it by replacing cards. The most common of these is Five-card draw.
         /// </summary>
-        Holdem,
+        DrawPoker,
+
+        /// <summary>
+        /// Games in which each player receives a combination of face-up cards and face-down cards in multiple betting rounds. The most common of these are Seven-card stud and Five-card stud.
+        /// </summary>
+        StudPoker,
+
+        /// <summary>
+        /// Games in which each player's incomplete hidden hand is combined with shared face-up cards. The most common of these are Texas hold 'em and Omaha hold 'em.
+        /// </summary>
+        CommunityCardsPoker,
+
     }
 }
